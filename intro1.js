7c7249d9a8378da6e4e657963674f183fc971dba
Minospeed007 <nwannemino@gmail.com>
2021-11-30 07:22:13 -0800

    first node commit

---
+++ intro1.js
@@ -0,0 +1,17 @@
+const amount = 12
+
+if (amount < 12){
+    console.log("small amount")
+}
+
+else{
+    console.log("large number")
+}
+
+console.log(`hey this is my first Node program`)
+
+console.log(__dirname)
+setInterval(()=>{
+    console.log("hello")
+}, 1000)
+console.log(__filename)
---
+++ mind-grenade.js
@@ -0,0 +1,7 @@
+const num1 = 5
+const num2 = 12
+
+function addValues(){
+    console.log(`the sum is : ${num1 + num2}`)
+}
+addValues()
